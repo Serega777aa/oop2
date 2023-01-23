04c12d844c949dee7c035a1b83cf1bd665277b61
Your Name <serega777aa@mail.ru>
2023-01-23 18:53:28 +0300

    seminar2

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona papa = new Persona("Ima", "F", 33, "lib1");
+        Persona son = new Persona("Sin", "F", 13, "lib2");
+
+        papa.PersLibr.AddBook("Пушкин", "Собрание сочинений", "RU");
+        papa.PersLibr.AddBook("Носов", "Незнайка на луне", "RU");
+
+        son.PersLibr.AddBook("Народное авторство", "Азбука", "RU");
+        son.PersLibr.AddBook("", "Light blue", "RU");
+        papa.AddName(son);
+        view.treeView(papa, 0);
+
+        System.out.println("*".repeat(15));
+        view.bookView(papa);
+        System.out.println("*".repeat(15));
+        view.bookView(son);
+        papa.Voice();
+
+    }
+}
